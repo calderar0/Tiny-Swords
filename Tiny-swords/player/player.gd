bf6d0d39ba7fc1424b6ff334c4c9db4a4e707604
calderar0 <felipecalderaro28@gmail.com>
2024-06-11 15:40:32 -0300

    Movimentação do personagem adicionada

--- Tiny-swords/player/player.gd
+++ Tiny-swords/player/player.gd
@@ -1,15 +1,42 @@
 extends CharacterBody2D
-
-
+ 
+@export var speed: float = 3
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
 var is_running: bool = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("move_up"):
+func _physics_process(delta: float) -> void:
+	#pega a direção e velocidade
+	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	#deadzone (isso aqui é por causa do controle pae)
+	var deadzone = 0.15
+	if abs(input_vector.x) < deadzone:
+		input_vector.x = 0
+	if abs(input_vector.y) < deadzone:
+		input_vector.y = 0
+		
+	#muda a velocidade
+	var target_velocity = input_vector * speed * 100
+	velocity = lerp(velocity,target_velocity, 0.05)
+	move_and_slide()
+	
+	#muda a var isrunning
+	var was_running = is_running
+	is_running = not input_vector.is_zero_approx()
+	
+	#animação
+	if was_running != is_running:
 		if is_running:
-			animation_player.play("idle")
-			is_running = false
-		else:
 			animation_player.play("run")
-			is_running = true
+		else:
+			animation_player.play("idle")
+	
+	#gira sprite
+	if input_vector.x > 0:
+		sprite.flip_h = false
+	elif input_vector.x < 0:
+		sprite.flip_h = true
+	
+	
