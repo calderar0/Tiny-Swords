89f3c53621a5b4c0d7d8a41757c07af0ae5e5203
calderar0 <felipecalderaro28@gmail.com>
2024-06-12 15:00:56 -0300

    Configurações de ataque adicionadas

--- Tiny-swords/player/player.gd
+++ Tiny-swords/player/player.gd
@@ -5,6 +5,17 @@ extends CharacterBody2D
 @onready var sprite: Sprite2D = $Sprite2D
 
 var is_running: bool = false
+var is_attacking: bool = false
+var attack_cooldown: float = 0.0
+
+func _process(delta: float) -> void:
+	#cooldown ataque
+	if is_attacking:
+		attack_cooldown -= delta
+		if attack_cooldown <= 0:
+			is_attacking = false
+			is_running = false
+			animation_player.play("idle")
 
 func _physics_process(delta: float) -> void:
 	#pega a direção e velocidade
@@ -19,6 +30,8 @@ func _physics_process(delta: float) -> void:
 		
 	#muda a velocidade
 	var target_velocity = input_vector * speed * 100
+	if is_attacking:
+		target_velocity *= 0.25
 	velocity = lerp(velocity,target_velocity, 0.05)
 	move_and_slide()
 	
@@ -27,16 +40,45 @@ func _physics_process(delta: float) -> void:
 	is_running = not input_vector.is_zero_approx()
 	
 	#animação
-	if was_running != is_running:
-		if is_running:
-			animation_player.play("run")
-		else:
-			animation_player.play("idle")
+	if not is_attacking:
+		if was_running != is_running:
+			if is_running:
+				animation_player.play("run")
+			else:
+				animation_player.play("idle")
 	
 	#gira sprite
 	if input_vector.x > 0:
 		sprite.flip_h = false
 	elif input_vector.x < 0:
 		sprite.flip_h = true
+	
+	#sistema de ataque
+	if Input.is_action_just_pressed("attack"):
+		attack(input_vector)
+	
+	
+	
+func attack(input_vector) -> void:
+	if is_attacking:
+		return
+	
+	#attack up 1
+	if input_vector.y < 0:
+		animation_player.play("attack_up1")
+		attack_cooldown = 0.6
+	elif input_vector.y > 0:
+		animation_player.play("attack_down1")
+		attack_cooldown = 0.6
+	else:
+	#attack side 1
+		animation_player.play("attack_side1")
+		attack_cooldown = 0.6
+	#attack side 2
+	
+	
+	
+	#marca ataque
+	is_attacking = true
 	
 	
