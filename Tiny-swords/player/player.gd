d7b14bd1ecbd7da54998dfcc5c6b54a3e669e18d
calderar0 <felipecalderaro28@gmail.com>
2024-06-19 10:24:39 -0300

    Adicionado item de regeneração de vida

--- Tiny-swords/player/player.gd
+++ Tiny-swords/player/player.gd
@@ -1,8 +1,10 @@
+class_name Player
 extends CharacterBody2D
  
 @export var speed: float = 3
 @export var sword_damage: int = 2
 @export var health: int = 100
+@export var max_health: int = 100
 @export var death_prefab: PackedScene
 
 @onready var sprite: Sprite2D = $Sprite2D
@@ -187,3 +189,10 @@ func die() -> void:
 		death_object.position = position
 		get_parent().add_child(death_object)
 	queue_free()
+
+func heal(amount: int) -> int:
+	health += amount
+	if health >= max_health:
+		health = max_health
+	print("Player curou ", amount, " vida atual: ", health, " de ", max_health)
+	return health
